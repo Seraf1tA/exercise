0b1901a55f142bb17dfdf997a3cc36ba2a32c856
seraf1ta-PC\seraf1ta <krzysztof.babiarz.seraf@gmail.com>
2017-10-05 14:09:59 +0100

    .

--- meanyMethodsAndInstances/src/meanymethodsandinstances/MeanyMethodsAndInstances.java
+++ meanyMethodsAndInstances/src/meanymethodsandinstances/MeanyMethodsAndInstances.java
@@ -1,21 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package meanymethodsandinstances;
 
-/**
- *
- * @author seraf1ta
- */
+import java.util.Scanner;
+
 public class MeanyMethodsAndInstances {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Scanner sc = new Scanner(System.in);
+        anotherClass ac = new anotherClass();
+        System.out.println("Enter name of first gf: ");
+        String t = sc.nextLine();
+        ac.setName(t);
+        ac.saying();
+        
     }
     
 }
--- meanyMethodsAndInstances/src/meanymethodsandinstances/anotherClass.java
+++ meanyMethodsAndInstances/src/meanymethodsandinstances/anotherClass.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package meanymethodsandinstances;
 
-/**
- *
- * @author seraf1ta
- */
 public class anotherClass {
+
+    private String girlName; //<- private visible only to this class method
+    public void setName(String name){
+    
+        girlName = name;
+    
+    }
+    public String getName(){
+        return girlName;
+    }
+    public void saying(){
+    
+        System.out.printf("Your first gf was %s", getName()); //<- put string in args
+    
+    }
     
 }
