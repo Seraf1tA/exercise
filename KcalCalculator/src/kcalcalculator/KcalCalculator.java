46092c19e1e12b65e32154aeb2b4195c887dc6a8
seraf1ta-PC\seraf1ta <krzysztof.babiarz.seraf@gmail.com>
2017-11-30 13:31:44 +0000

    no message

--- KcalCalculator/src/kcalcalculator/KcalCalculator.java
+++ KcalCalculator/src/kcalcalculator/KcalCalculator.java
@@ -7,14 +7,14 @@ public class KcalCalculator {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        double waga, wynik, wysilek;
+        double weight, sum, intensity;
         int mnoznik = 24 ;
         System.out.println("Enter your wight: ");
-        waga = sc.nextDouble();
+        weight = sc.nextDouble();
         System.out.println("Enter intensity of your days from 1 to 1.5 ");
-        wysilek = sc.nextDouble();
-        wynik = waga * mnoznik * wysilek;
-        System.out.println("Your kcal is: " + wynik);
+        intensity = sc.nextDouble();
+        sum = weight * mnoznik * intensity;
+        System.out.println("Your kcal is: " + sum);
     
     }
     
