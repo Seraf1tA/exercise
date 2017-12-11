99667d0b9fec71650faa7327b323b035174809c7
seraf1ta-PC\seraf1ta <krzysztof.babiarz.seraf@gmail.com>
2017-12-11 17:01:51 +0000

    no message

--- JComboBox/src/jcombobox/Gui.java
+++ JComboBox/src/jcombobox/Gui.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jcombobox;
 
-/**
- *
- * @author seraf1ta
- */
-public class Gui {
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.JComboBox;
+
+public class Gui extends JFrame{
+
+    private JComboBox box;
+    private JLabel picture;
     
+    private static String[] filename = {"a.jpg", "b.jpg"};
+    private Icon[] pics = {new ImageIcon(getClass().getResource(filename[0])), new ImageIcon(getClass().getResource(filename[1]))};
+    
+    public Gui(){
+    
+        super("the title");
+        setLayout(new FlowLayout());
+        
+        box = new JComboBox(filename);
+        
+        
+    
+    }
 }
