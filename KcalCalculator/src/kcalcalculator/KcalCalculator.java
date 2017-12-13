2c4f976765b1ece331d691a6ea5e6af3f07e253a
seraf1ta-PC\seraf1ta <krzysztof.babiarz.seraf@gmail.com>
2017-12-13 17:14:22 +0000

    no message

--- KcalCalculator/src/kcalcalculator/KcalCalculator.java
+++ KcalCalculator/src/kcalcalculator/KcalCalculator.java
@@ -1,6 +1,5 @@
 package kcalcalculator;
 
-import java.util.Scanner;
 import javax.swing.JFrame;
 
 public class KcalCalculator {
@@ -9,14 +8,8 @@ public class KcalCalculator {
         
         Gui g = new Gui();
         g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        g.setSize(500, 200);
         g.setVisible(true);
        
-        //Cal ca = new Cal();
-        
-        
-        
-    
     }
     
 }
