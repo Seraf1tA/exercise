a9712c48434cb54fbfab842a116637d3b8a82dd4
seraf1ta-PC\seraf1ta <krzysztof.babiarz.seraf@gmail.com>
2017-12-13 15:17:22 +0000

    Revert "no message"
    This reverts commit 49c233323c36bbb8710fa2586e8c928033d7e999.

---
+++ KcalCalculator/src/kcalcalculator/KcalCalculator.java
@@ -0,0 +1,26 @@
+package kcalcalculator;
+
+import java.util.Scanner;
+import javax.swing.JFrame;
+
+public class KcalCalculator {
+
+    public static void main(String[] args) {
+        
+        Gui g = new Gui();
+        g.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        g.setSize(500, 200);
+        g.setVisible(true);
+       
+        //Cal ca = new Cal();
+        
+        
+        
+    
+    }
+    
+}
+
+
+ 
+
